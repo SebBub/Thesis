cdf2e8c29c901377b242dd689c502df29d18a492
Sebastian Bubmann <sb1234@posteo.de>
2023-02-03 10:51:58 +0100

    Major update to forecast verification notebook.

--- Dataprocessing.py
+++ Dataprocessing.py
@@ -1,9 +1,38 @@
 import xarray as xr
 from pathlib import Path
 import logging
+from tqdm import tqdm
 from climpred.preprocessing.shared import set_integer_time_axis
 import numpy as np
 
+
+def get_daily_tp_rate(path, outfolder, globpat=None, var="tp"):
+    path = Path(path)
+    outfolder = Path(outfolder)
+
+    if not outfolder.exists():
+        outfolder.mkdir()
+
+    dataarrays = []
+
+    if (globpat == None):  # single file
+        da = xr.open_dataset(path, chunks="auto")[var]
+        dataarrays.append(da)
+
+    else:  # list of files
+        files = list(path.glob(globpat))
+        logging.info(f"Reading {len(files)} files into memory:")
+        for file in tqdm(files):
+            da = xr.open_dataset(file, chunks="auto")[var]
+            dataarrays.append(da)
+
+    logging.info(f"\n Processing {len(dataarrays)} DataArrays:")
+    for c, dataarray in enumerate(tqdm(dataarrays)):  # see https://confluence.ecmwf.int/pages/viewpage.action?pageId=197702790
+        attrs = dataarray.attrs
+        da = dataarray.diff("time") # attrs are dropped
+        da.attrs = attrs
+        da = xr.concat([dataarray.isel({"time":0}), da], dim="time")
+        da.to_netcdf(outfolder / (files[c].parts[-1][:-3] + "_disaggregated.nc"))
 
 def crop_netCDF(path, lat1, lat2, lon1, lon2, globpat=None, to_netcdf=False):
     """
@@ -25,34 +54,39 @@ def crop_netCDF(path, lat1, lat2, lon1, lon2, globpat=None, to_netcdf=False):
 
     path = Path(path)
     cropped_path = path / "Cropped"
-    logging.info(f"Testing if path {cropped_path} existis.")
+
     if to_netcdf and not cropped_path.exists():
         cropped_path.mkdir()
         logging.info(f"Created directory {cropped_path} \n")
 
-    if globpat == None: # single file
+    datasets = []
+    if globpat == None:  # single file
         ds = xr.open_dataset(path, chunks="auto")
-    else:
-        ds = xr.open_mfdataset(path.glob(globpat), chunks="auto")
+        datasets.append(ds)
 
+    else:  # note: open_mfdataset() does not work because no monotonic indexes to concatenate along for SEAS5 hindcasts -> use loop
+        filelist = list(path.glob(globpat))
+        logging.info("Reading multiple datasets:")
+        for file in tqdm(filelist):
+            ds = xr.open_dataset(file, chunks="auto")
+            datasets.append(ds)
+
+    logging.info("Cropping multiple datasets:")
+    for count, dataset in enumerate(tqdm(datasets)):
         try:
-            logging.info("Trying slice with dim names 'latitude'&'longitude'")
-            ds_crop = ds.sel({"latitude": y_slice}).sel({"longitude": x_slice})
+            logging.info("\n Trying slice with dim names 'latitude'&'longitude'")
+            ds_crop = dataset.sel({"latitude": y_slice}).sel({"longitude": x_slice})
         except:
-            logging.info("That didn't work. Trying slice with dim names "
-                         "'lat'&'lon'")
-            ds_crop = ds.sel({"lat": y_slice}).sel({"lon": x_slice})
+            logging.info("That didn't work. Trying slice with dim names 'lat'&'lon'")
+            ds_crop = dataset.sel({"lat": y_slice}).sel({"lon": x_slice})
 
         if to_netcdf:
-            filename = path.parts[-1][:-3] + "_box.nc"
-            logging.info(f"Writing file {filename} to {cropped_path}")
-            ds_crop.to_netcdf(
-                cropped_path / filename
-            )
+            filename = filelist[count].parts[-1][:-3] + "_box.nc"
+            ds_crop.to_netcdf(cropped_path / filename)
+            logging.info(f"Wrote file {filename} to {cropped_path} \n")
 
 
 def ERA5_hourly_daily(path, globpat=None, to_netcdf=False):
-
     if globpat == None:
         path = Path(path)
         ds = xr.open_dataset(path, chunks="auto")
@@ -69,8 +103,11 @@ def ERA5_hourly_daily(path, globpat=None, to_netcdf=False):
                 ds.to_netcdf(file.parent / (file.parts[-1][:-3] + "_daily.nc"))
 
 
-def prep_hindcast(path, globpat, leadtime, to_netcdf=False, outfolder=None,
-                  filename="hc.nc"):
+def prep_hindcast(path, globpat, to_netcdf=False, filename="prepped_hindcast.nc"):
+    """
+    Be careful with the selection of the index of the timestamp for the init dimension! When the data was diff()ed from accumulated to daily tp rate, this needs
+    to be shifted one up!
+    """
     path = Path(path)
     outfolder = path / "climpred_hindcast"
     logging.info(f"Checking if {outfolder} exists.")
@@ -79,22 +116,21 @@ def prep_hindcast(path, globpat, leadtime, to_netcdf=False, outfolder=None,
         logging.info(f"Folder {outfolder} created.\n")
 
     files = list(path.glob(globpat))
-    logging.info(f"Processing multiple files {files}.")
+    logging.info(f"Processing {len(files)} files.")
     datasets = []
-    for file in files:
-        # ds = xr.open_dataset(file, chunks="auto")
-        ds = xr.open_dataset(file).isel({"time": leadtime}) #pick leadtime
-        # ds = ds.rename({"number": "member"})
-        # ds = ds.assign_coords({"init": (ds.time.values[0] - np.timedelta64(1, "D"))})
-        #.expand_dims("init") #TODO check if we need to actively expand the dims
-        # ds = set_integer_time_axis(ds)
-        # ds = ds.rename({"time": "lead"})
-        # ds.lead.attrs["units"] = "days"
+    for file in tqdm(files):
+        ds = xr.open_dataset(file, chunks="auto")
+        ds = ds.rename({"number": "member"})
+        ds = ds.assign_coords({"init": (ds.time.values[0] - np.timedelta64(1, "D"))}).expand_dims("init")
+        ds = set_integer_time_axis(ds)
+        ds = ds.rename({"time": "lead"})
+        ds.lead.attrs["units"] = "days"
         datasets.append(ds)
-    hc = xr.concat(datasets, dim="time")
+    hc = xr.concat(datasets, dim="init")
 
     if to_netcdf:
         fname = outfolder / filename
-        hc.to_netcdf()
+        hc.to_netcdf(fname)
         logging.info(f"Wrote {filename} to folder {outfolder} \n")
+
     return hc
